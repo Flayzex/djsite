31db7ac85a8039d20a72e99ae3209c6ddf79dc09
Flayzex <redkiyflayzex@gmail.com>
2023-08-13 12:24:11 +0500

    first

---
+++ coolsite/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from coolsite import settings
+from women.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
+    path('', include('women.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
